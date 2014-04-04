d5a82cfa2919e7ab901848dcd33f42ef1e7d8360
dessoya <dessoya@gmail.com>
2014-04-04 10:46:29 +0400

    gitignore

--- liteposix/build/Release/.deps/Release/obj.target/module.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/module.node := flock ./Release/linker.lock g++ -shared -pthread -rdynamic -m64  -Wl,-soname=module.node -o Release/obj.target/module.node -Wl,--start-group Release/obj.target/module/module.o -Wl,--end-group 
